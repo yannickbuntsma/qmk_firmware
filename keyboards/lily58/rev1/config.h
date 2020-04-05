8aae72930cc48386fb644b513286d550e3b0fe4b
yannickbuntsma <yannickbuntsma@gmail.com>
2020-04-05 16:36:14 +0200

    map(initial): dupe settings, flip pins, change serial pin

--- keyboards/lily58/rev1/config.h
+++ keyboards/lily58/rev1/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFC51
-#define PRODUCT_ID      0x0058 
+#define PRODUCT_ID      0x0058
 #define DEVICE_VER      0x0100
 #define MANUFACTURER    F_YUUCHI
 #define PRODUCT         Lily58
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { C6, D7, E6, B5, B4 } // flipped B4 and B5 due to miswire
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
 /* define tapping term */
